eda1176d3fd9fdee264821ff3dde930ccd37bd82
kamalkiran898 <70885339+kamalkiran898@users.noreply.github.com>
2020-10-18 02:57:37 -0700

    Add files via upload

---
+++ email_validationrepo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Email_data_Validation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string email;
+            Console.WriteLine("Enter your email id");
+            email = Console.ReadLine();
+            if (IsValidEmail(email))
+            {
+                Console.WriteLine("entered email is valid");
+            }
+            else
+                Console.WriteLine("entered email is not valid");
+
+            Console.ReadLine();
+        }
+
+        public static bool IsValidEmail(string email)
+        {
+
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+            try
+            {
+                if (Regex.IsMatch(email,
+                        @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                        RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250)))
+                {
+                    return true;
+                }
+                else
+                    return false;
+            }
+            catch (Exception )
+            {
+                return false;
+            }
+            
+        }
+    }
+}
+
