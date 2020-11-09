ad8381aa94c080febbeebc24c72caf19a8c6acc0
kamalkiran898 <70885339+kamalkiran898@users.noreply.github.com>
2020-11-09 00:44:18 -0800

    CONTACTS_SEARCHING

---
+++ searching_contacts.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace contact_searching
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.text1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_search = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // text1
+            // 
+            this.text1.Location = new System.Drawing.Point(402, 89);
+            this.text1.Name = "text1";
+            this.text1.Size = new System.Drawing.Size(100, 20);
+            this.text1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(243, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "enter contacts";
+            // 
+            // button_search
+            // 
+            this.button_search.Location = new System.Drawing.Point(607, 89);
+            this.button_search.Name = "button_search";
+            this.button_search.Size = new System.Drawing.Size(75, 23);
+            this.button_search.TabIndex = 2;
+            this.button_search.Text = "search";
+            this.button_search.UseVisualStyleBackColor = true;
+            this.button_search.Click += new System.EventHandler(this.button_search_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(301, 158);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(309, 225);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button_search);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.text1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox text1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_search;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ searching_contacts.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace contact_searching
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_search_Click(object sender, EventArgs e)
+        {
+            string searchtext = text1.Text;
+            if (searchtext == null || searchtext == "")
+            {
+                MessageBox.Show("search string is empty");
+            }
+            else
+            {
+                StreamReader streamReader = new StreamReader("C:\\Users\\NARASIMHARAO\\Desktop\\csharp\\Kiran.txt");
+                string temptext = streamReader.ReadLine();
+                while (temptext != "")
+                {
+                    if (searchtext.Equals(temptext))
+                    {
+                        listBox1.Items.Add(searchtext);
+                        listBox1.Items.Add(streamReader.ReadLine());
+                        break;
+                    }
+                    temptext = streamReader.ReadLine();
+                }
+            }
+        }
+    }
+}
